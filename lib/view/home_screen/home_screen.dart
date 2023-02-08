8f45ee36573a7e99a6c90244504a4eea7ecb810d
Vishwas <vishwaskumar445@gmail.com>
2023-02-08 21:51:35 +0530

    new fixes

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_post/utils/app_colors.dart';
 import 'package:instagram_post/view/auth/auth_screen.dart';
-import 'package:instagram_post/view/notification/activity_screen.dart';
+import 'package:instagram_post/view/notification/notification_screen.dart';
 import 'package:instagram_post/view/profile/profile_screen.dart';
 import 'package:instagram_post/view/recent/recent_post_screen.dart';
 import 'package:instagram_post/view/search_screen.dart/search_screen.dart';
